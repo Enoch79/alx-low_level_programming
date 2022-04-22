61f51fcf4c1983e787df73f4de67c8f3e2f34675
Enoch79 <linanar8@gmail.com>
2022-04-22 06:29:40 -0700

    updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,9 @@
-   global    main
+global    main
           extern    printf
 main:
-  mov   edi, format
-  xor   eax, eax
-  call  printf
-  mov eax, 0
-  ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
 format: db `Hello, Holberton\n`,0
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs

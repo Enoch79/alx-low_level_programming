65674a2343f35319815daa5587d00224d6be5471
Enoch79 <linanar8@gmail.com>
2022-06-17 15:18:54 -0700

    work

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -0 /tmp/win.so https://github.com/Enoch79/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
-wxport LD_PRELOAD=/tmp/win.so
+wget -P /tmp https://github.com/Enoch79/alx-low_level_programming/raw/main/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
